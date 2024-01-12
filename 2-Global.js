f0a9be323861be0f915b6fff20e5a6115c4d8321
MUKUL <mukul.2226csit1115@kiet.edu>
2024-01-12 23:53:26 +0530

    OS-Module

--- 2-Global.js
+++ 2-Global.js
@@ -3,5 +3,5 @@ console.log(__filename)
 setInterval(()=>{
         console.log("Hello world")
 },1000)
-This is the way we understand Global 
-This thing is very important wrt to Node.js
+// This is the way we understand Global 
+// This thing is very important wrt to Node.js
--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-//Modules
-const names = require('./4-names');
-const sayHi = require('./5-utils');
-const data = require('./6-alternative-flavour')
-// console.log(data)
-require('./7-mind-grenade')
-// sayHi('susan')
-// sayHi(names.john)
-// sayHi(names.peter)
+const os = require('os')
+//info about  current user
+const user = os.userInfo()
+console.log(user)
+
+//method returns the system uptime in seconds
+console.log(`They system uptime is ${os.uptime()} second `)
+
+const currentOS ={
+    name:os.type(),
+    release:os.release(),
+    totalMem: os.totalmem(),
+    freeMem : os.freemem(),
+}
+console.log(currentOS)
