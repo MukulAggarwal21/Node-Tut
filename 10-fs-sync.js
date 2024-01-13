b289a33b9cae7229e86144b81631b99194df8c83
MUKUL <mukul.2226csit1115@kiet.edu>
2024-01-13 12:51:52 +0530

    Sync Vs Async

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -7,4 +7,8 @@ const second = readFileSync('./content/subfolder/second.txt' , 'utf8')
 // console.log(first , second)
 
 // a means append 
-writeFileSync('./content/result-sync.txt ' , `Hey there is the result:  ${first} , ${second}` , {flag : 'a'})
+writeFileSync('./content/result-sync.txt ' , `Hey there is the result:  ${first} , ${second}` , {flag : 'a'}
+)
+
+console.log('done with the Task')
+console.log('Starting with the next one')
--- 11-fs-async.js
+++ 11-fs-async.js
@@ -1,6 +1,6 @@
 const {readFileSync, writeFileSync, readFile, writeFile } = require('fs')
 
-
+console.log('start')
 // return callback whenever the fnction is called
 readFileSync('./content/first.txt' , 'utf8' , (err, result)=>{
     if(err){
@@ -14,12 +14,15 @@ readFileSync('./content/first.txt' , 'utf8' , (err, result)=>{
        return
     }
      const second = result
-     writeFile('./content/result-async.txt' , `Here is the result: ${first}, ${second}`, (err,result)=>{
+     writeFile('./content/result-async.txt' , `Here is the result: ${first}, ${second}`,
+      (err,result)=>{
         if(err){
             console.log(err)
             return
         }
-        console.log(result)
+        console.log('Done With the Task')
+        // console.log(result)
      })
    })
 })
+console.log('starting with next task')
