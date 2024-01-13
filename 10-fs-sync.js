d9143e39cf4e4427fe5b478ebdb2fd6c6e5a8dd0
MUKUL <mukul.2226csit1115@kiet.edu>
2024-01-13 10:47:22 +0530

    Fs-sync-Module

---
+++ 10-fs-sync.js
@@ -0,0 +1,10 @@
+// fs is module name 
+const {readFileSync, writeFileSync } = require('fs')
+// for encoding file
+const first = readFileSync('./content/first.txt' , 'utf8')
+const second = readFileSync('./content/subfolder/second.txt' , 'utf8')
+
+// console.log(first , second)
+
+// a means append 
+writeFileSync('./content/result-sync.txt ' , `Hey there is the result:  ${first} , ${second}` , {flag : 'a'})
