7d28b80e4ba736be43a40a6341d3ef307039f620
MUKUL <mukul.2226csit1115@kiet.edu>
2024-01-14 23:58:27 +0530

    req res- tut

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+const http = require('http');
 
+const server = http.createServer((req , res)=> {
+    console.log(req)
+  res.write('Welcome to our Home page')
+  res.end()
+
+})
+
+server.listen(5000)
