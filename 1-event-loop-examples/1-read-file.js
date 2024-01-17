bd2dfc0efc1b7db56d2b3aced4392aae657f0ad1
MUKUL <mukul.2226csit1115@kiet.edu>
2024-01-17 23:47:59 +0530

    Event Loop

--- 1-event-loop-examples/1-read-file.js
+++ 1-event-loop-examples/1-read-file.js
@@ -1,10 +1,3 @@
-// const _ = require('lodash')
-// //  one of lodash method  make it as a flat array as follows: 
-// const items = [1,[2,[3,[4]]]]
-// const newItems = _.flattenDeep(items)
-// console.log(newItems)
-// console.log('hello people');
-
 const { readFile, writeFile } = require('fs')
 console.log('started a first task')
 // CHECK FILE PATH!!!!
