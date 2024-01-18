cc2a799a2222eed891e1d48f12d283174a3f9ffe
MUKUL <mukul.2226csit1115@kiet.edu>
2024-01-18 23:42:10 +0530

    Blocking Code Updated

--- app.js
+++ app.js
@@ -4,27 +4,3 @@
 // const newItems = _.flattenDeep(items)
 // console.log(newItems)
 // console.log('hello people');
-
-const http = require('http');
-
-const server = http.createServer((req,res)=> {
-if(req.url === '/'){
-  res.end('Home Page')
-}
-else if(req.url==='/about'){
-  //Blocking Code!!: it is nested for loop
-  for(let i=0 ; i<1000 ; i++){
-    for(let j=0 ;j<1000 ; j++ ){
-      console.log(`${i} ${j}`)
-    }
-  }
-  res.end('About Page')
-}
-else{
-  res.end('Error Page')
-}
-
-})
-server.listen(5000,()=>{
-  console.log('Server is Listening on port 5000...')
-})
